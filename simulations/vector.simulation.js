320d4b0335dfdec3264d423dca78f31ca3faa68f
mafritz <mattia.fritz@unige.ch>
2020-02-07 20:20:05 +0100

    Rebuilt simulation scripts

--- simulations/vector.simulation.js
+++ simulations/vector.simulation.js
@@ -1,67 +1,33 @@
 const rewire = require("rewire");
 const DEW = rewire("../dist/dew.lib").__get__("DEW");
 const fs = require("fs");
+const { parse } = require("json2csv");
 
-const affectiveSpace = {
-  id: "ry-s0AORKG",
-  lang: "en",
-  name: "Geneva Emotion Wheel version 3 (vector)",
-  description:
-    "The 20 emotions used in the third version of the Geneva Emotion Wheel",
-  algorithmType: "vector",
-  dimX: "Valence",
-  dimY: "Power",
-  isGrid: true,
-  isPublic: true,
-  user: "admin",
-  createdAt: "2018-03-20T12:30:11.200Z",
-  updatedAt: "2018-05-28T10:51:54.991Z",
-  feelings: [
-    { label: "Admiration", valueX: 66, valueY: 9 },
-    { label: "Amusement", valueX: 67, valueY: 19 },
-    { label: "Anger", valueX: -37, valueY: 47 },
-    { label: "Compassion", valueX: -5, valueY: -55 },
-    { label: "Contempt", valueX: -55, valueY: 43 },
-    { label: "Contentment", valueX: 77, valueY: 3 },
-    { label: "Disappointment", valueX: -77, valueY: -12 },
-    { label: "Disgust", valueX: -68, valueY: 20 },
-    { label: "Fear", valueX: -61, valueY: 7 },
-    { label: "Guilt", valueX: -57, valueY: -27 },
-    { label: "Hate", valueX: -45, valueY: 43 },
-    { label: "Interest", valueX: 61, valueY: 25 },
-    { label: "Joy", valueX: 68, valueY: 7 },
-    { label: "Love", valueX: 58, valueY: -16 },
-    { label: "Pleasure", valueX: 71, valueY: 2 },
-    { label: "Pride", valueX: 72, valueY: 15 },
-    { label: "Regret", valueX: -70, valueY: -19 },
-    { label: "Relief", valueX: 66, valueY: -36 },
-    { label: "Sadness", valueX: -68, valueY: -35 },
-    { label: "Shame", valueX: -61, valueY: -16 }
-  ],
-  _id: 3,
-  citation:
-    "Scherer, K. R., Shuman, V., Fontaine, J. R. J., & Soriano, C. (2013). The GRID meets the Wheel: Assessing emotional feeling via self-report. In J. R. J. Fontaine, K. R. Scherer, & C. Soriano (Eds.), Components of Emotional Meaning: A sourcebook (pp. 281–298). Oxford, UK: Oxford University Press."
-};
+const fields = ["x", "y", "label", "order"];
+const opts = { fields };
+
+const affectiveSpace = DEW.affectiveSpacesList.find(function(space) {
+  return space.name == "Geneva Emotion Wheel version 3 (vector)";
+});
 
 let simulatedData = [];
 for (let x = -100; x <= 100; x++) {
   for (let y = -100; y <= 100; y++) {
     let simulation = DEW.sortAffectiveSpace(affectiveSpace, x, y);
-    let pivot_longer = simulation.map(function(feeling, index) {
-      return {
+    simulation.forEach(function(feeling, index) {
+      simulatedData.push({
         x: x,
         y: y,
-        position: index + 1,
-        label: feeling.label
-      };
+        label: feeling.label,
+        order: index + 1
+      });
     });
-    simulatedData.push(pivot_longer);
   }
 }
 
-console.log(simulatedData);
-
-fs.writeFileSync(
-  __dirname + "/data/gew_vector.json",
-  JSON.stringify(simulatedData)
-);
+try {
+  const csv = parse(simulatedData, opts);
+  fs.writeFileSync(__dirname + "/data/simulation_gew_vector.csv", csv);
+} catch (err) {
+  console.error(err);
+}
